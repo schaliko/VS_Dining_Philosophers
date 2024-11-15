55ee00dcca58233deb82c81bfcfeaec4201a621b
schaliko <116072699+schaliko@users.noreply.github.com>
2024-11-15 13:22:56 +0000

    Blatt2 fertig

--- main.c
+++ main.c
@@ -14,124 +14,47 @@ union semun {
 } argument;
 
 
-
-// // Funktion zum Aufnehmen der Gabeln
-// void pick_up_forks(int sem_id, int left, int right) {
-//     // Struktur für Semaphoroperationen
-//     struct sembuf sem_op;
-//     // Atomare Operation, um beide Gabeln aufzunehmen
-//     sem_op.sem_num = left;
-//     sem_op.sem_op = -1; // Linke Gabel nehmen
-//     sem_op.sem_flg = 0;
-//     // semop(sem_id, &sem_op, 1);
-//     if (semop(sem_id, &sem_op, 1) < 0) {
-//         perror("Error in P() operation0");
-//         exit(1);
-//     }
-
-//     sem_op.sem_num = right;
-//     sem_op.sem_op = -1; // Rechte Gabel nehmen
-//     // semop(sem_id, &sem_op, 1);
-//     if (semop(sem_id, &sem_op, 1) < 0) {
-//         perror("Error in P() operation1");
-//         exit(1);
-//     }
-// }
-
 void pick_up_forks(int sem_id, int left, int right) {
     struct sembuf ops[2];
 
-    // Configure the first operation to lock the left fork
-    ops[0].sem_num = left;   // Semaphore number for the left fork
-    ops[0].sem_op = -1;      // Decrement by 1 (take the fork)
-    ops[0].sem_flg = 0;      // No special flags
+    // left fork
+    ops[0].sem_num = left;
+    ops[0].sem_op = -1;      
+    ops[0].sem_flg = 0;      
 
-    // Configure the second operation to lock the right fork
-    ops[1].sem_num = right;  // Semaphore number for the right fork
-    ops[1].sem_op = -1;      // Decrement by 1 (take the fork)
-    ops[1].sem_flg = 0;      // No special flags
+    // right fork
+    ops[1].sem_num = right; 
+    ops[1].sem_op = -1;
+    ops[1].sem_flg = 0;
 
-    // Perform the atomic semaphore operation on both forks
     if (semop(sem_id, ops, 2) < 0) {
         perror("Error in atomic P() operation for picking up forks");
         exit(1);
     }
 }
 
-
-
-
-// // Funktion zum Ablegen der Gabeln
-// void put_down_forks(int sem_id, int left, int right) {
-//     // Struktur für Semaphoroperationen
-//     struct sembuf sem_op;
-//     sem_op.sem_num = left;
-//     sem_op.sem_op = 1; // Linke Gabel ablegen
-//     sem_op.sem_flg = 0;
-//     // semop(sem_id, &sem_op, 1);
-//     if (semop(sem_id, &sem_op, 1) < 0) {
-//         perror("Error in P() operation2");
-//         exit(1);
-//     }
-
-//     sem_op.sem_num = right;
-//     sem_op.sem_op = 1; // Rechte Gabel ablegen
-//     // semop(sem_id, &sem_op, 1);
-//     if (semop(sem_id, &sem_op, 1) < 0) {
-//         perror("Error in P() operation3");
-//         exit(1);
-//     }
-// }
-
 void put_down_forks(int sem_id, int left, int right) {
     struct sembuf ops[2];
 
-    // Configure the first operation to release the left fork
+    // left fork
     ops[0].sem_num = left;
-    ops[0].sem_op = 1;       // Increment by 1 (release the fork)
+    ops[0].sem_op = 1;       
     ops[0].sem_flg = 0;
 
-    // Configure the second operation to release the right fork
+    // right fork
     ops[1].sem_num = right;
-    ops[1].sem_op = 1;       // Increment by 1 (release the fork)
+    ops[1].sem_op = 1;       
     ops[1].sem_flg = 0;
 
-    // Perform the atomic semaphore operation to release both forks
     if (semop(sem_id, ops, 2) < 0) {
         perror("Error in atomic V() operation for putting down forks");
         exit(1);
     }
 }
 
-
-// Prozessfunktion für Philosophen
-void philosopher(int phil_id, int sem_id) {
-    int left = phil_id;
-    int right = (phil_id + 1) % NUM_PHILOSOPHERS;
-
-    while (1) {
-        // Philosoph denkt
-        printf("Philosopher %d is thinking.\n", phil_id);
-        sleep(rand() % 3); // Zufällige Denkzeit
-
-        // Gabeln aufnehmen
-        printf("Philosopher %d is hungry and tries to pick up forks.\n", phil_id);
-        pick_up_forks(sem_id, left, right);
-
-        // Philosoph isst
-        printf("Philosopher %d is eating.\n", phil_id);
-        sleep(rand() % 3); // Zufällige Essenszeit
-
-        // Gabeln ablegen
-        put_down_forks(sem_id, left, right);
-        printf("Philosopher %d has finished eating and put down forks.\n", phil_id);
-    }
-}
-
 int main() {
     int sem_id;
     pid_t pid;
-    // int status;
     key_t sem_key;
 
     if ((sem_key = ftok("./semfile.txt", '1')) < 0) {
@@ -154,29 +77,44 @@ int main() {
         }
     }   
 
-    // Erstellen der Philosophenprozesse
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        pid = fork();
-        if (pid == 0) {
-            // Kindprozess führt Philosophen-Aktion aus
-            srand(getpid()); // Initialisieren des Zufallszahlengenerators
-            philosopher(i, sem_id);
-            exit(0);
-        } else if (pid < 0) {
-            perror("Fehler beim Erstellen des Prozesses");
-            exit(1);
+        switch (fork()) {
+            case -1:
+                perror("Fork failed");
+                exit(1);
+
+            case 0:  // Child process
+                srand(getpid()); // Initialisieren des Zufallszahlengenerators
+                // philosopher(i, sem_id);
+                int left = i;
+                int right = (i + 1) % NUM_PHILOSOPHERS;
+
+                while (1) {
+                    // Philosoph denkt
+                    printf("Philosopher %d is thinking.\n", i);
+                    sleep(rand() % 3);
+
+                    // Gabeln aufnehmen
+                    printf("Philosopher %d is hungry and tries to pick up forks.\n", i);
+                    pick_up_forks(sem_id, left, right);
+
+                    // Philosoph isst
+                    printf("Philosopher %d is eating.\n", i);
+                    sleep(rand() % 3); 
+
+                    // Gabeln ablegen
+                    put_down_forks(sem_id, left, right);
+                    printf("Philosopher %d has finished eating and put down forks.\n", i);
+                }
+                exit(0);
+            default:
+                break;
         }
     }
 
-    // Wait for all philosopher processes to complete
+    // wait for philosopher
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
         wait(NULL);
-    }
-
-    // Löschen der Semaphoren
-    if (semctl(sem_id, 0, IPC_RMID) == -1) {
-        perror("Fehler beim Löschen der Semaphoren");
-        exit(1);
     }
 
     return 0;
